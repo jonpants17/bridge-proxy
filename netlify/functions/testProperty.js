f0cfeedad1c911e9ddf8ff8056208e80104f051d
jonpants17 <jonvillalta@gmail.com>
2026-06-30 11:04:36 -0600

    Update testProperty.js

--- netlify/functions/testProperty.js
+++ netlify/functions/testProperty.js
@@ -1,49 +1,71 @@
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
-exports.handler = async function (event) {
+exports.handler = async function () {
   const { BRIDGE_API_KEY } = process.env;
-  const mls = (event.queryStringParameters?.mls || "").trim();
-
-  if (!mls) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({
-        success: false,
-        error: "Missing mls parameter",
-      }),
-    };
-  }
 
   const url =
-    `https://api.bridgedataoutput.com/api/v2/OData/rae/Property` +
-    `?$filter=(ListingId eq '${mls}')` +
-    `&$top=1`;
+    "https://api.bridgedataoutput.com/api/v2/OData/rae/Property" +
+    "?$filter=MLSNumber eq 'E4494960'" +
+    "&$top=1";
 
   try {
-    const response = await fetch(url, {
+    const res = await fetch(url, {
       headers: {
         Authorization: `Bearer ${BRIDGE_API_KEY}`,
         Accept: "application/json",
       },
     });
 
-    const text = await response.text();
+    const json = await res.json();
+
+    const listing = json.value?.[0];
+
+    if (!listing) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ success: false, message: "Listing not found" }, null, 2),
+      };
+    }
 
     return {
-      statusCode: response.status,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: text,
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          success: true,
+
+          MLSNumber: listing.MLSNumber,
+          ListingId: listing.ListingId,
+          ListingKey: listing.ListingKey,
+
+          ListAgentFullName: listing.ListAgentFullName,
+          ListAgentFirstName: listing.ListAgentFirstName,
+          ListAgentLastName: listing.ListAgentLastName,
+
+          ListAgentKey: listing.ListAgentKey,
+          ListAgentMlsId: listing.ListAgentMlsId,
+
+          ListOfficeName: listing.ListOfficeName,
+          ListOfficeKey: listing.ListOfficeKey,
+
+          StandardStatus: listing.StandardStatus,
+          BridgeModificationTimestamp: listing.BridgeModificationTimestamp
+        },
+        null,
+        2
+      ),
     };
   } catch (err) {
     return {
       statusCode: 500,
-      body: JSON.stringify({
-        success: false,
-        error: err.message,
-      }),
+      body: JSON.stringify(
+        {
+          success: false,
+          error: err.message,
+        },
+        null,
+        2
+      ),
     };
   }
 };
