8853b4acb8cae591def69280d49460d350d2d4e0
jonpants17 <jonvillalta@gmail.com>
2026-06-30 11:09:49 -0600

    Update getFeatured.js

--- netlify/functions/getFeatured.js
+++ netlify/functions/getFeatured.js
@@ -5,13 +5,8 @@ const fetch = (...args) =>
 
 const ODATA_BASE = "https://api.bridgedataoutput.com/api/v2/OData/rae";
 
-function safeMLS(raw) {
-  return String(raw || "").trim().toUpperCase().replace(/[^A-Z0-9]/g, "");
-}
-
-function escapeODataString(v) {
-  return String(v || "").replace(/'/g, "''");
-}
+const CAITLYN_AGENT_KEY = "0522a4120745c4d8ed95045061a1f165";
+const MAXWELL_OFFICE_KEY = "8f49787f3d74882c79e028e7e1c69139";
 
 function isDisplayable(l) {
   return l?.IDXParticipationYN !== false && l?.RAE_L_IdxInclude !== "No";
@@ -39,17 +34,15 @@ exports.handler = async function () {
   };
 
   try {
-    const featuredId = safeMLS(process.env.FEATURED_IDS || "E4494960");
-    const officeName = "MaxWell Progressive";
-
     const primaryUrl =
       `${ODATA_BASE}/Property` +
-      `?$filter=(ListingId eq '${featuredId}')` +
+      `?$filter=(ListAgentKey eq '${CAITLYN_AGENT_KEY}' and IDXParticipationYN eq true)` +
+      `&$orderby=BridgeModificationTimestamp desc` +
       `&$top=1`;
 
     const officeUrl =
       `${ODATA_BASE}/Property` +
-      `?$filter=(ListOfficeName eq '${escapeODataString(officeName)}' and IDXParticipationYN eq true)` +
+      `?$filter=(ListOfficeKey eq '${MAXWELL_OFFICE_KEY}' and IDXParticipationYN eq true)` +
       `&$orderby=BridgeModificationTimestamp desc` +
       `&$top=6`;
 
@@ -62,7 +55,7 @@ exports.handler = async function () {
 
     const listings = officeResults
       .filter(isDisplayable)
-      .filter((l) => l.ListingId !== featuredId)
+      .filter((l) => !primary || l.ListingKey !== primary.ListingKey)
       .slice(0, 3);
 
     return {
